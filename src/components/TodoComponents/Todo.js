38e9dc4498643ff1bf6b52c67a35c0a784cbb900
Kyle Richardson <kyle.richardson1@gmail.com>
2019-10-05 12:41:50 -0500

    clicking works for children spans

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // eslint-disable-next-line
-import { faTrash, faCheck} from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faCheck, faMonument} from '@fortawesome/free-solid-svg-icons'
+import moment from "moment"
 
 const Todo = (props) => {
     let list = ''
@@ -15,12 +16,15 @@ const Todo = (props) => {
                     <div key={item.id} className={`single-item`}>
                         <div className={`left-side ${item.completed ? "checked":null}`} onClick={props.handleCheck} name={item.id}>
                             <div className="checkmark">{item.completed ? <FontAwesomeIcon icon={faCheck}/> : " "}</div>
-                            {item.task}
+                                <span>
+                                {item.task}
+                                </span>
+                                <span style={{fontSize: '1rem', marginLeft: '20%'}}>{item.completed ? `(completed on ${moment().format('lll')})` : null}</span>
+                            </div>
+                        <div className="delete" name={item.id} onClick={props.handleDelete}>
+                            <FontAwesomeIcon className="trash-icon" icon={faTrash}/>
+                            
                         </div>
-                        <span className="delete" name={item.id} onClick={props.handleDelete}>
-                            {/* <FontAwesomeIcon className="trash-icon" icon={faTrash}/> */}
-                            X
-                        </span>
                     </div>
                 )
             })}
